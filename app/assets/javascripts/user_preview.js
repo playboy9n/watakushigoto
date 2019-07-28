f70bfb3bd53b2e815c5436e39ca4411a774d7cb8
playboy9n <playboy9n@gmail.com>
2019-07-28 12:19:41 +0000

    プレビュー試行錯誤２

--- app/assets/javascripts/user_preview.js
+++ app/assets/javascripts/user_preview.js
@@ -1,11 +1,11 @@
-$(document).on('turbolinks:load', function(){
-  $('.attach').on('change', function(e) {
-    let file = e.target.files[0];
-    // ファイルのブラウザ上でのURLを取得する
-    let blobUrl = window.URL.createObjectURL(file);
-    // img要素に表示
-    $('.preview_cicle').attr('src', blobUrl);
-  });
-});
+// $(document).on('turbolinks:load', function(){
+//   $('.attach').on('change', function(e) {
+//     let file = e.target.files[0];
+//     // ファイルのブラウザ上でのURLを取得する
+//     let blobUrl = window.URL.createObjectURL(file);
+//     // img要素に表示
+//     $('.preview_cicle').attr('src', blobUrl);
+//   });
+// });
 
 
